bc5297ecff6bd8f2d32aab0fdd34d0f1e96ce2dc
Benito Sanchez <sanchezbenitog@gmail.com>
2016-10-28 17:06:00 -0700

    adding code to read json
    Summary:
    adding functionality to read json code.
    Hit an endpoint
    Display result on screen
    
    Test Plan:
    Patch to mentorme
    run mentorme
    Verify that a list of dummy values displays on subjects screen
    
    Reviewers: Salv95
    
    Reviewed By: Salv95
    
    Differential Revision: http://csumbse.net/D123

--- MentorMe/app/src/main/java/mentorme/csumb/edu/mentorme/login/homeScreen/HomeActivity.java
+++ MentorMe/app/src/main/java/mentorme/csumb/edu/mentorme/login/homeScreen/HomeActivity.java
@@ -1,22 +1,118 @@
 package mentorme.csumb.edu.mentorme.login.homeScreen;
 
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import mentorme.csumb.edu.mentorme.R;
+import mentorme.csumb.edu.mentorme.login.webRequest.WebRequest;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends ListActivity {
+
+    private static String url = "https://mentorme-api.appspot.com/_ah/api/helloworld/v1/hellogreeting";
+
+    private static final String ITEMS = "items";
+    private static final String MESSAGE = "message";
+    private static final String KIND = "kind";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        new GetSubjects().execute();
+    }
+
+    private class GetSubjects extends AsyncTask<Void, Void, Void> {
+
+        ArrayList<HashMap<String, String>> listOfSubjects;
+
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            progressDialog = new ProgressDialog(HomeActivity.this);
+            progressDialog.setMessage("Please wait...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            WebRequest webRequest = new WebRequest();
+
+            String jsonString = webRequest.makeWebServiceCall(url, WebRequest.GET);
+            Log.d("Response: ", " > " + jsonString);
+
+            listOfSubjects = ParseJSON(jsonString);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            if (progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    HomeActivity.this, listOfSubjects,
+                    R.layout.subjects_list, new String[] {MESSAGE, KIND},
+                    new int[]{R.id.message, R.id.kind});
+
+            setListAdapter(adapter);
+        }
+
+        private ArrayList<HashMap<String, String>> ParseJSON(String json) {
+            if (json != null) {
+                try {
+                    ArrayList<HashMap<String, String>> studentList = new ArrayList<>();
+
+                    JSONObject jsonObject = new JSONObject(json);
+
+                    JSONArray students = jsonObject.getJSONArray(ITEMS);
+
+                    for (int i = 0; i < students.length(); i++) {
+                        JSONObject c = students.getJSONObject(i);
+
+                        String message = c.getString(MESSAGE);
+                        String kind = c.getString(KIND);
+
+                        HashMap<String, String> testSubjects = new HashMap<>();
+
+                        testSubjects.put(MESSAGE, message);
+                        testSubjects.put(KIND, kind);
+
+                        studentList.add(testSubjects);
+                    }
+                    return studentList;
+                } catch (JSONException e) {
+                    e.printStackTrace();;
+                    return null;
+                }
+            } else {
+                Log.e("Service Handler", "Couldn't get any data from url");
+                return null;
+            }
+        }
     }
 
 }
+
+
