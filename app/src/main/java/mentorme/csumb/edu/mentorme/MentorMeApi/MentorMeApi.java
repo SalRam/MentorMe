9013aec85cb49ebcafc4d867b5cee09b0341c63e
Benito Sanchez <sanchezbenitog@gmail.com>
2016-10-31 16:49:25 -0700

    Refactoring endpoint call with retrofit
    Summary: refactored endpoint call by using retrofit library
    
    Test Plan:
    patch to mentorme
    run project
    verify that Subjects screen displays a list of mocked data
    
    Reviewers: Salv95
    
    Reviewed By: Salv95
    
    Differential Revision: http://csumbse.net/D156

---
+++ app/src/main/java/mentorme/csumb/edu/mentorme/MentorMeApi/MentorMeApi.java
@@ -0,0 +1,36 @@
+package mentorme.csumb.edu.mentorme.MentorMeApi;
+
+import mentorme.csumb.edu.mentorme.data.model.Subjects;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+/**
+ * MentorMe Api.
+ */
+public interface MentorMeApi {
+
+    String BASE_URL = "https://mentorme-api.appspot.com/";
+
+    @GET("_ah/api/helloworld/v1/hellogreeting/")
+    Call<Subjects> getSubjects();
+
+    class Factory {
+
+        private static MentorMeApi service;
+
+        public static MentorMeApi getInstance() {
+            if (service == null) {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .baseUrl(BASE_URL)
+                        .build();
+
+                return retrofit.create(MentorMeApi.class);
+            } else {
+                return service;
+            }
+        }
+    }
+}
