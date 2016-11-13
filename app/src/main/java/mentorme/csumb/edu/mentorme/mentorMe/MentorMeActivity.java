bae977945305c6819d9067b27b63b5b8513c4136
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-12 21:21:35 -0800

    Refactoring layout
    Summary:
    Refactoring toolbar layout
    
    Adding translucent status bar
    Making toolbar reach to scroll
    
    Test Plan:
    patch to mentorme
    lauch mentorme
    verify that status bar is transparent
    verify that when scrolled down, toolbar moves upwards
    
    Reviewers: Salv95
    
    Differential Revision: http://csumbse.net/D300

--- app/src/main/java/mentorme/csumb/edu/mentorme/mentorMe/MentorMeActivity.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/mentorMe/MentorMeActivity.java
@@ -1,9 +1,17 @@
 package mentorme.csumb.edu.mentorme.mentorMe;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import mentorme.csumb.edu.mentorme.R;
 
 /**
  * MentorMeActivity will be a super activity that initializes default set up for each activity.
