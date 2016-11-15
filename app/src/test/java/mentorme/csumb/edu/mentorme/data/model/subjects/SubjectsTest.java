08d0a935f614631da7946c6ba3e6cf6d9aee14fd
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-14 19:03:06 -0800

    adding tests to Subjects
    Summary:
    Adding tests to Subjects
    Test Plan:
    patch to mentorme
    open SubjectsTest
    run file
    verify that tests pass
    Reviewers:
    salv95
    Subscribers:

---
+++ app/src/test/java/mentorme/csumb/edu/mentorme/data/model/subjects/SubjectsTest.java
@@ -0,0 +1,80 @@
+package mentorme.csumb.edu.mentorme.data.model.subjects;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by benitosanchez on 11/14/16.
+ */
+@RunWith(JUnit4.class)
+public class SubjectsTest {
+
+    private final String FIRST_SUBJECT = "Algorythms";
+    private final String SECOND_SUBJECT = "Math";
+    private final String THIRD_SUBJECT = "EnglisH";
+
+    private Subject mSubject;
+    private Subjects mSubjects;
+    private ArrayList<Subject> mListOfSubjects;
+
+    @Before
+    public void setUp() {
+        mSubjects = new Subjects();
+        mListOfSubjects = new ArrayList<>();
+    }
+
+    @Test
+    public void getSubjects_whenSubjectsMatch_shouldPass() {
+        // Given
+        mSubject = new Subject();
+        mSubject.setSubject(FIRST_SUBJECT);
+        mListOfSubjects.add(mSubject);
+
+        mSubject = new Subject();
+        mSubject.setSubject(SECOND_SUBJECT);
+        mListOfSubjects.add(mSubject);
+
+        mSubjects.setSubjects(mListOfSubjects);
+
+        // When
+        ArrayList<Subject> list = mSubjects.getSubjects();
+
+        // Then
+        assertEquals(list, mListOfSubjects);
+    }
+
+    @Test
+    public void getSubjects_whenSubjectsDoNotMatch_shouldFail() {
+        // Given
+        mSubject = new Subject();
+        mSubject.setSubject(FIRST_SUBJECT);
+        mListOfSubjects.add(mSubject);
+
+        mSubject = new Subject();
+        mSubject.setSubject(THIRD_SUBJECT);
+        mListOfSubjects.add(mSubject);
+
+        mSubjects.setSubjects(mListOfSubjects);
+
+        mSubject = new Subject();
+        mSubject.setSubject(SECOND_SUBJECT);
+
+        // When
+        ArrayList<Subject> list = new ArrayList<>();
+        list.add(mSubject);
+
+        // Then
+        assertFalse(list.equals(mListOfSubjects));
+    }
+}
