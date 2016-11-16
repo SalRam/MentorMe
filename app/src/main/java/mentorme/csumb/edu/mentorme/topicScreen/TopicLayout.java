a9acb9eec2b0311c89ca3744c0d15e7f7df5f284
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-16 15:08:50 -0800

    Adding back arrow instead of Nav menu. Adding on error layout to topics
    Summary:
    Removed navigation menu to topics
    Adding back arrow to tool bar
    Showing the network error screen on topics
    
    Test Plan:
    patch to mentorme
    launch mentorme
    On subjects menu
    Click on a Subject
    Verify that a back arrow shows on topics screen
    Click on back arrow
    Verify that Subjects activity launches
    
    Go to airplane mode
    Click on a Subject
    Verify that topics screen shows a sad cloud
    
    Reviewers: Salv95
    
    Differential Revision: http://csumbse.net/D399

--- app/src/main/java/mentorme/csumb/edu/mentorme/topicScreen/TopicLayout.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/topicScreen/TopicLayout.java
@@ -1,13 +1,12 @@
 package mentorme.csumb.edu.mentorme.topicScreen;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -15,7 +14,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import mentorme.csumb.edu.mentorme.R;
 import mentorme.csumb.edu.mentorme.data.model.topics.Topics;
-import mentorme.csumb.edu.mentorme.homeScreen.homeLayoutAdapter.SubjectsAdapter;
 import mentorme.csumb.edu.mentorme.topicScreen.topicsLayoutAdapter.TopicsAdapter;
 import rx.Subscriber;
 
@@ -26,36 +24,30 @@ public class TopicLayout extends Subscriber<Topics> {
 
     private final String TAG = "TopicLayout";
 
-    private AppCompatActivity mActivity;
+    private TopicActivity mActivity;
     private TopicLayoutListener mListener;
 
-    @BindView(R.id.drawer_layout) DrawerLayout mDrawer;
-    @BindView(R.id.nav_view) NavigationView mNavigationView;
     @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
-    @BindView(R.id.toolbar_layout) Toolbar mToolbar;
+    @BindView(R.id.toolbar_no_menu) Toolbar mToolbar;
     @BindView(R.id.toolbar_title) TextView mToolbarTitle;
+    @BindView(R.id.toolbar_back_arrow) ImageView mBackArrow;
+    @BindView(R.id.network_error_layout) LinearLayout mNetworkErrorLayout;
 
-    TopicLayout(AppCompatActivity activity, TopicLayoutListener listener) {
+    TopicLayout(TopicActivity activity, TopicLayoutListener listener) {
         mActivity = activity;
         mListener = listener;
 
-        mActivity.setContentView(R.layout.app_main_layout);
+        mActivity.setContentView(R.layout.topics_layout);
 
         ButterKnife.bind(this, mActivity);
 
-        mToolbarTitle.setText("Topics");
+        mToolbarTitle.setText(R.string.topics);
         mActivity.setSupportActionBar(mToolbar);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                mActivity, mDrawer, mToolbar, R.string.open, R.string.close);
-        mDrawer.addDrawerListener(toggle);
-        toggle.syncState();
     }
 
-    @OnClick(R.id.nav_view)
-    public void onNavigationMenuClick(){
-
-        mListener.onNavigationMenuClick();
+    @OnClick(R.id.toolbar_back_arrow)
+    public void onBackArrowClicked() {
+        mActivity.onBackPressed();
     }
 
     @Override
@@ -66,6 +58,7 @@ public class TopicLayout extends Subscriber<Topics> {
     @Override
     public void onError(Throwable e) {
         Log.d(TAG, e.getMessage());
+        mNetworkErrorLayout.setVisibility(View.VISIBLE);
     }
 
     @Override
