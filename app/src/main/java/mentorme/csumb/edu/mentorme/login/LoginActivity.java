e8022e96b71ba38c1470415dee0e568d94fd21ee
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-21 19:28:35 -0800

    Refacoring Google Sign in model to try to fix bug
    Summary:
    Trying to fix google sign in bug
    Refactored Gogole Sign Model
    
    Test Plan:
    Patch to mentorme
    launch mentorme through apk file
    verify that log in works
    
    Reviewers: Salv95
    
    Differential Revision: http://csumbse.net/D420

--- app/src/main/java/mentorme/csumb/edu/mentorme/login/LoginActivity.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/login/LoginActivity.java
@@ -2,12 +2,18 @@ package mentorme.csumb.edu.mentorme.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
+import mentorme.csumb.edu.mentorme.R;
+import mentorme.csumb.edu.mentorme.login.googleSignInModel.GoogleApiSignInModel;
 import mentorme.csumb.edu.mentorme.mentorMe.MentorMeActivity;
 
 /**
@@ -18,7 +24,6 @@ public class LoginActivity extends MentorMeActivity {
     private static final String TAG = "SignInActivity";
     private static final int RC_SIGN_IN = 9001;
 
-    private GoogleApiClient mGoogleApiClient;
     private LoginController mLoginController;
 
     @Override
@@ -28,7 +33,6 @@ public class LoginActivity extends MentorMeActivity {
         // ** Needed for Fabric **//
         Fabric.with(this, new Crashlytics());
 
-        mGoogleApiClient = getGoogleApiClient();
         mLoginController = new LoginController(this);
         ButterKnife.bind(this);
     }
@@ -37,7 +41,7 @@ public class LoginActivity extends MentorMeActivity {
     public void onStart() {
         super.onStart();
 
-        mLoginController.initialSubscriber(mGoogleApiClient);
+        mLoginController.initialSubscriber();
     }
 
     @Override
