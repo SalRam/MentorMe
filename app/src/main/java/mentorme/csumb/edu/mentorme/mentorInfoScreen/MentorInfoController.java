815bd4ea20304bc2e02097166a1e7e5b24876f6e
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-30 02:12:37 -0800

    Adding dagger to all controllers
    Summary:
    Adding Dagger to all controller
    Test Plan:
    patch to mentorme
    launch mentorme
    verify that everything works
    Reviewers:
    salv95
    Subscribers:

--- app/src/main/java/mentorme/csumb/edu/mentorme/mentorInfoScreen/MentorInfoController.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/mentorInfoScreen/MentorInfoController.java
@@ -1,6 +1,15 @@
 package mentorme.csumb.edu.mentorme.mentorInfoScreen;
 
-import mentorme.csumb.edu.mentorme.mentorMeApi.mentorMeImpl.Factory;
+import javax.inject.Inject;
+
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+import mentorme.csumb.edu.mentorme.MentorMeApp;
+import mentorme.csumb.edu.mentorme.data.component.NetComponent;
+import mentorme.csumb.edu.mentorme.mentorMeApi.MentorMeApi;
+import mentorme.csumb.edu.mentorme.util.PerController;
+import retrofit2.Retrofit;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -11,19 +20,46 @@ import rx.schedulers.Schedulers;
 public class MentorInfoController {
 
     private MentorInfoActivity mActivity;
-    private MentorInfoLayout mMentorInfoLayout;
+    @Inject MentorInfoLayout mMentorInfoLayout;
+    @Inject Retrofit mRetrofit;
 
     MentorInfoController(MentorInfoActivity activity) {
         mActivity = activity;
-        mMentorInfoLayout = new MentorInfoLayout(mActivity);
+
+        DaggerMentorInfoController_MentorInfoComponent.builder()
+                .netComponent(((MentorMeApp) mActivity.getApplicationContext()).getNetComponent())
+                .mentorInfoModule(new MentorInfoModule(mActivity))
+                .build()
+                .inject(this);
 
         onAttach();
     }
 
     private void onAttach() {
-        Factory.getInstance().getMentorInformation()
+        mRetrofit.create(MentorMeApi.class).getMentorInformation()
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(mMentorInfoLayout);
+    }
+
+    @PerController
+    @Component(dependencies = NetComponent.class, modules = MentorInfoModule.class)
+    interface MentorInfoComponent {
+        void inject(MentorInfoController mentorInfoController);
+    }
+
+    @Module
+    static class MentorInfoModule {
+        private final MentorInfoActivity mActivity;
+
+        public MentorInfoModule(MentorInfoActivity activity) {
+            mActivity = activity;
+        }
+
+        @Provides
+        @PerController
+        MentorInfoLayout providesMentorInfoLayout() {
+            return new MentorInfoLayout(mActivity);
+        }
     }
 }
--- app/src/main/java/mentorme/csumb/edu/mentorme/topicScreen/TopicController.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/topicScreen/TopicController.java
@@ -1,8 +1,15 @@
 package mentorme.csumb.edu.mentorme.topicScreen;
 
-import android.support.v7.app.AppCompatActivity;
+import javax.inject.Inject;
 
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+import mentorme.csumb.edu.mentorme.MentorMeApp;
+import mentorme.csumb.edu.mentorme.data.component.NetComponent;
 import mentorme.csumb.edu.mentorme.mentorMeApi.mentorMeImpl.Factory;
+import mentorme.csumb.edu.mentorme.util.PerController;
+import retrofit2.Retrofit;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -12,11 +19,18 @@ import rx.schedulers.Schedulers;
 public class TopicController implements TopicLayout.TopicLayoutListener {
 
     private TopicActivity mActivity;
-    private TopicLayout mTopicLayout;
+
+    @Inject TopicLayout mTopicLayout;
+    @Inject Retrofit mRetrofit;
 
     TopicController(TopicActivity activity) {
         mActivity = activity;
-        mTopicLayout = new TopicLayout(mActivity, this);
+
+        DaggerTopicController_TopicControllerComponent.builder()
+                .netComponent(((MentorMeApp) mActivity.getApplicationContext()).getNetComponent())
+                .topicControllerModule(new TopicControllerModule(mActivity, this))
+                .build()
+                .inject(this);
 
         onAttach();
     }
@@ -30,4 +44,28 @@ public class TopicController implements TopicLayout.TopicLayoutListener {
 
     @Override
     public void onNavigationMenuClick() { }
+
+    @PerController
+    @Component(dependencies = NetComponent.class, modules = TopicControllerModule.class)
+    interface TopicControllerComponent {
+        void inject(TopicController topicController);
+
+    }
+
+    @Module
+    static class TopicControllerModule {
+        private final TopicActivity mActivity;
+        private final TopicLayout.TopicLayoutListener mListener;
+
+        public TopicControllerModule(TopicActivity activity, TopicLayout.TopicLayoutListener listener) {
+            mActivity = activity;
+            mListener = listener;
+        }
+
+        @Provides
+        @PerController
+        TopicLayout providesTopicLayout() {
+            return new TopicLayout(mActivity, mListener);
+        }
+    }
 }
