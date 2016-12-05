732b2721897c6a5aa6d8909e856b1b0a4b7fd74e
Benito Sanchez <sanchezbenitog@gmail.com>
2016-12-05 15:07:48 -0800

    Fixed bug in search bar and refactored code.
    Summary: Fixed bug in search bar and refactored logic in layout to controller
    
    Test Plan:
    patch to mentorme
    
    launch mentorme
    verify that keyboard does not open on home screen by default
    click on search layout
    verify that keyboard shows up
    verify that blinking cursor is not present.
    type "c"
    verify that subjects get updated.
    delete "c"
    verify that subjects get updated.
    type " "
    verify that nothing is updated
    
    Reviewers: Salv95
    
    Reviewed By: Salv95
    
    Differential Revision: http://csumbse.net/D658

--- app/src/main/java/mentorme/csumb/edu/mentorme/homeScreen/HomeController.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/homeScreen/HomeController.java
@@ -2,7 +2,11 @@ package mentorme.csumb.edu.mentorme.homeScreen;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
@@ -13,6 +17,7 @@ import mentorme.csumb.edu.mentorme.MentorMeApp;
 import mentorme.csumb.edu.mentorme.R;
 import mentorme.csumb.edu.mentorme.addingMentor.AddingMentorActivity;
 import mentorme.csumb.edu.mentorme.data.component.NetComponent;
+import mentorme.csumb.edu.mentorme.data.model.subjects.Subject;
 import mentorme.csumb.edu.mentorme.mentorMeApi.MentorMeApi;
 import mentorme.csumb.edu.mentorme.util.PerController;
 import retrofit2.Retrofit;
@@ -22,12 +27,16 @@ import rx.schedulers.Schedulers;
 /**
  * Controller for the {@link HomeActivity}.
  */
-public class HomeController implements HomeLayout.HomeLayoutListener {
+public class HomeController
+        implements HomeLayout.HomeLayoutListener,
+        TextWatcher {
 
     private HomeActivity mActivity;
+    private ArrayList<Subject> mSubjects;
 
     @Inject HomeLayout mHomeLayout;
     @Inject Retrofit mRetrofit;
+
 
     public HomeController(@NonNull HomeActivity activity) {
 
@@ -35,7 +44,7 @@ public class HomeController implements HomeLayout.HomeLayoutListener {
 
         DaggerHomeController_HomeComponent.builder()
                 .netComponent(((MentorMeApp) mActivity.getApplicationContext()).getNetComponent())
-                .homeControllerModule(new HomeControllerModule(mActivity, this))
+                .homeControllerModule(new HomeControllerModule(mActivity, this, this))
                 .build()
                 .inject(this);
 
@@ -47,11 +56,20 @@ public class HomeController implements HomeLayout.HomeLayoutListener {
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(mHomeLayout);
+
+        mSubjects = mHomeLayout.getSubjects();
     }
     @Override
     public void onNavigationMenuClick() { }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
+    /**
+     * Handles menu item click.
+     *
+     * @param item Menu item clicked.
+     *
+     * @return {@true} if item exists. {@false} otherwise.
+     */
+    boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add_mentor:
                 Intent intent = new Intent(mActivity, AddingMentorActivity.class);
@@ -62,6 +80,36 @@ public class HomeController implements HomeLayout.HomeLayoutListener {
                 return false;
         }
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        String string  = s.toString().toLowerCase();
+        ArrayList<Subject> mFilteredList = new ArrayList<>();
+
+        mSubjects = mHomeLayout.getSubjects();
+
+        if (!string.trim().isEmpty()) {
+            if (!mSubjects.isEmpty() && mSubjects != null) {
+
+                for (int i =0; i < mSubjects.size(); i++) {
+                    final String subjectTile = mSubjects.get(i).getSubject().toLowerCase();
+                    if (subjectTile.contains(s)) {
+                        mFilteredList.add(mSubjects.get(i));
+                    }
+                }
+            }
+        } else {
+            mFilteredList = mSubjects;
+        }
+
+        mHomeLayout.notifyTextChanged(mFilteredList);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) { }
 
     /**
      * Component for {@link HomeController}
@@ -80,16 +128,21 @@ public class HomeController implements HomeLayout.HomeLayoutListener {
     static class HomeControllerModule {
         private final HomeActivity mActivity;
         private final HomeLayout.HomeLayoutListener mListener;
+        private final HomeController mController;
 
-        public HomeControllerModule(HomeActivity activity, HomeLayout.HomeLayoutListener listener) {
+        public HomeControllerModule(
+                HomeActivity activity,
+                HomeLayout.HomeLayoutListener listener,
+                HomeController controller) {
             mActivity = activity;
             mListener = listener;
+            mController = controller;
         }
 
         @Provides
         @PerController
         HomeLayout providesHomeLayout() {
-            return new HomeLayout(mActivity, mListener);
+            return new HomeLayout(mActivity, mListener, mController);
         }
     }
 
