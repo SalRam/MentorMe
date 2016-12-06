8746ccc22214dc4303d06ecc8d78b4e76784e946
Benito Sanchez <sanchezbenitog@gmail.com>
2016-12-05 18:15:21 -0800

    Adding searchbar to all layouts
    Summary:
    Adding search bar to all layout
    run tests that were needed on previous search bar to these search bars.
    
    Test Plan:
    patch to mentorme
    test search bars to same steps are previous search bar on:
    - Topics screen
    - Mentors screen
    Reviewers:
    salv95
    Subscribers:

--- app/src/main/java/mentorme/csumb/edu/mentorme/topicScreen/TopicLayout.java
+++ app/src/main/java/mentorme/csumb/edu/mentorme/topicScreen/TopicLayout.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,8 +19,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import mentorme.csumb.edu.mentorme.R;
+import mentorme.csumb.edu.mentorme.data.model.subjects.Subject;
 import mentorme.csumb.edu.mentorme.data.model.topics.Topic;
 import mentorme.csumb.edu.mentorme.data.model.topics.Topics;
+import mentorme.csumb.edu.mentorme.homeScreen.HomeLayout;
+import mentorme.csumb.edu.mentorme.homeScreen.homeLayoutAdapter.SubjectsAdapter;
 import mentorme.csumb.edu.mentorme.mentorScreen.MentorActivity;
 import mentorme.csumb.edu.mentorme.topicScreen.topicsLayoutAdapter.TopicsAdapter;
 import mentorme.csumb.edu.mentorme.views.layout.toolbar.ToolbarSupport;
@@ -38,12 +42,17 @@ class TopicLayout
     private TopicActivity mActivity;
     private ArrayList<Topic> mTopics;
     private ToolbarSupport mToolbar;
+    private TopicsAdapter mAdapter;
+    private TopicController mController;
+    private ArrayList<Topic> mFilteredList;
 
-    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
     @BindView(R.id.network_error_layout) LinearLayout mNetworkErrorLayout;
+    @BindView(R.id.search_text) EditText mSearchText;
+    @BindView(R.id.recycler_view) RecyclerView mRecyclerView;
 
-    TopicLayout(TopicActivity activity) {
+    TopicLayout(TopicActivity activity, TopicController controller) {
         mActivity = activity;
+        mController = controller;
         mActivity.setContentView(R.layout.topics_layout);
         mToolbar = new BackArrowToolbar(mActivity, R.string.topics);
 
@@ -64,13 +73,9 @@ class TopicLayout
     @Override
     public void onNext(@NonNull Topics topics) {
         mTopics = topics.getTopics();
-        TopicsAdapter adapter = new TopicsAdapter(
-                mActivity.getApplicationContext(),
-                mTopics,
-                this);
-
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity.getApplicationContext()));
-        mRecyclerView.setAdapter(adapter);
+        mFilteredList = mTopics;
+        initList();
+        mSearchText.addTextChangedListener(mController);
     }
 
     @Override
@@ -85,8 +90,38 @@ class TopicLayout
         }
 
         intent.putExtra("subjectId", subjectId);
-        intent.putExtra("topicId", mTopics.get(position).getId());
+        intent.putExtra("topicId", mFilteredList.get(position).getId());
 
         mActivity.startActivity(intent);
+    }
+
+    void notifyTextChanged(ArrayList<Topic> filteredList) {
+        mFilteredList = filteredList;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity.getApplicationContext()));
+        mAdapter = new TopicsAdapter(mActivity.getApplicationContext(),
+                mFilteredList,
+                this);
+
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Returns a list of topics.
+     *
+     * @return The list of mentors.
+     */
+    public ArrayList<Topic> getTopics() {
+        return mTopics;
+    }
+
+    private void initList() {
+        mAdapter = new TopicsAdapter(
+                mActivity.getApplicationContext(),
+                mFilteredList,
+                this);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity.getApplicationContext()));
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
